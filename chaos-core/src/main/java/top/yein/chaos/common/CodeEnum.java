ffc5ec698cf9043ff9ad4580cce7b9e9e258eec4
kk70 <kevinz@weghst.com>
2020-11-20 16:18:39 +0800

    update

---
+++ chaos-core/src/main/java/top/yein/chaos/common/CodeEnum.java
@@ -0,0 +1,17 @@
+package top.yein.chaos.common;
+
+/**
+ * 带有枚举码的枚举.
+ *
+ * @author Kevin Zou (kevinz@weghst.com)
+ * @date 2020-11-20 15:27
+ */
+public interface CodeEnum {
+
+  /**
+   * 返回枚举码.
+   *
+   * @return 枚举码
+   */
+  int code();
+}
